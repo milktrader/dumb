5c03090a9f534cd65ebea0b8938cea0a31a3726a
Milktrader <milktrader@gmail.com>
2011-05-08 17:22:01 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dumb::Application.config.secret_token = 'a74e4686abafd97d738cf82b083520e62ea8ac52cb92bd150758b4db95c0147fd2d8d104942c59a0f0f2c83b9e581e3ab36f01671d103d4191441034ba5f013e'

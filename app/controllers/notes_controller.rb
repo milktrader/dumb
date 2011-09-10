e82d2295f31ee2b8cf8716e8b6d4310c7c9bb54f
Milktrader <milktrader@gmail.com>
2011-09-10 14:21:45 -0400

    seventh version

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -4,29 +4,38 @@ class NotesController < ApplicationController
   end
 
   def new
+    @title = "My Dumb Website | New Index"
   end
 
-  def git
+  def git 
+    @title = "My Dumb Website | git Index"
   end
 
   def rspec
+    @title = "My Dumb Website | Rspec Index"
   end
 
   def model
+    @title = "My Dumb Website | Model Index"
   end
 
   def controller
+    @title = "My Dumb Website | Controller Index"
   end
 
   def view
+    @title = "My Dumb Website | View Index"
   end
 
   def css
+    @title = "My Dumb Website | CSS Index"
   end
 
   def heroku
+    @title = "My Dumb Website | Heroku Index"
   end
 
   def todo
+    @title = "My Dumb Website | Todo Index"
   end
 end
